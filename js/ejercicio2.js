ab22d15bc020a0b022bd3a1736d10b22c8474829
tongaf <fornigastonfacu@hotmail.com>
2023-06-12 20:25:16 -0300

    ejer1 realizado ejer2 dudas

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,10 +1,9 @@
-var usuario = prompt("ingrese su usuario por favor")
-var contraseña = prompt("ingrese su contraseña por favor")
-var ValidacionUsuario = "admin"
-var ValidacionContraseña = 1234
+var creditos = [
+    {"plan" : "plan001" , "capital" : 150000 , "plazo" : 30 , "tasa" : 15},
+    {"plan" : "plan002" , "capital" : 300000 , "plazo" : 180 , "tasa" : 10},
+    {"plan" : "plan003" , "capital" : 485000 , "plazo" : 60 , "tasa" : 23}
+]
 
-if ( usuario == ValidacionUsuario && contraseña ==  ValidacionContraseña) {
-    alert("bienvenido")
-} else {
-    alert("error, usted no es el usuario")
+for ( var i=0 ; i<creditos.length ; i++) {
+    alert (" la tasa de interes del " + creditos[i].plan + " es " + (((creditos[i].capital * creditos[i].plazo) *creditos[i].tasa / 100 ) / 100 ))
 }
