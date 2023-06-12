f072c39b1fb72ffb34e2d1158f35f3a617cb4406
tongaf <fornigastonfacu@hotmail.com>
2023-06-11 23:05:44 -0300

    realizados los ejercicios m1

---
+++ js/ejercicio1.js
@@ -0,0 +1,6 @@
+
+var costoFijo = prompt("ingrese su costo fijo")
+var venta = prompt("ingrese su precio de venta")
+var costoVar = prompt("ingrese su costo variable")
+var resultado = costoFijo /  (venta - costoVar); 
+alert( "el resultado de su operacion es " + resultado )
---
+++ js/ejercicio2.js
@@ -0,0 +1,7 @@
+var presupuesto = prompt("ingrese su presupuesto")
+var resultado = presupuesto * 50 / 100 ; 
+alert( "Sus gastos necesarios son " + resultado )
+var resultado2 = presupuesto * 30 / 100 ;
+alert( "Sus gastos opcionales son " + resultado2 )
+var resultado3 = presupuesto * 20 / 100;
+alert( "Sus ahorros e inversion son " + resultado3 )
